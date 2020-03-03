d731f4e60bbcef8919de014b45f9b502a2fe7191
l17002876 <maxleclere1999@gmail.com>
2020-03-03 14:43:46 +0100

    ajout login php et js

--- frontend/login.php
+++ frontend/login.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <?= require_once 'head.php' ?>
+        <?php require_once 'head.php' ?>
         <script src="assets/javascript/login.js"></script>
     </head>
     <body>
-        <?= require_once 'header.php'?>
+        <?php require_once 'header.php'?>
         <main id="main">
             <h1>Connexion</h1>
             <form id="form_login" action="../backend/json/loginjs.php" method="post">
                 <label>Login</label>
-                <input type="text" name="login" required/>
+                <input type="text" name="login" required/><br>
                 <label>Password</label>
-                <input type="password" name="password" required/>
+                <input type="password" name="password" required/><br>
                 <input type="submit"/>
-                <a href="registration.php">Vous n'avez pas de compte? Inscriver-vous ici.</a>
             </form>
+            <a href="registration.php">Vous n'avez pas de compte? Inscriver-vous ici.</a>
         </main>
     </body>
 </html>
--- frontend/registration.php
+++ frontend/registration.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="fr">
     <head>
-        <?= require_once 'head.php' ?>
+        <?php require_once 'head.php' ?>
     </head>
     <body>
-        <?= require_once 'header.php'?>
+        <?php require_once 'header.php'?>
         <main id="main">
             <h1>Inscription</h1>
             <form id="form_registration" action="../backend/json/registrationjs.php" method="post">
                 <label>Login</label>
-                <input type="text" name="login" required/>
+                <input type="text" name="login" required/><br>
                 <label>Password</label>
-                <input type="password" name="password" required/>
+                <input type="password" name="password" required/><br>
                 <input type="submit"/>
             </form>
             <a href="login.php">Vous avez un compte? Connectez-vous ici.</a>
