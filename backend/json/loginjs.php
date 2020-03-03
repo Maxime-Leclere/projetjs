7fc77cab54e3a323d7dc8d61d0e38dbf6fe66fd3
maxime-leclere <maxleclere1999@gmail.com>
2020-03-03 16:00:01 +0100

    ajout login php et js

--- backend/json/loginjs.php
+++ backend/json/loginjs.php
@@ -4,20 +4,22 @@ session_start();
 $obj = new stdClass();
 $obj->success = false;
 $obj->message = 'nom d\'utilisateur ou mot de passe incorrect...';
-$login = $__POST['login'];
-$password = $__POST['password'];
-
 $found = null;
-// php a rempli $__POST[username] et $__POST[password]
-if($login == "momo" && $password == "lolo") {
-    $found = true; // trouver en base et ok
-} else {
-    $found = false;
-}
+if (isset($_POST['login'] && isset($_POST['password']) {
+    $login = $_POST['login'];
+    $password = $_POST['password'];
 
-if ($found) {
-    $obj->success = true;
-    $__SESSION['user'] = 123; // normalement c'est $__POST[username]
+    // php a rempli $__POST[username] et $__POST[password]
+    if($login == "momo" && $password == "lolo") {
+        $found = true; // trouver en base et ok
+    } else {
+        $found = false;
+    }
+
+    if ($found) {
+        $obj->success = true;
+        $_SESSION['user'] = 123; // normalement c'est $__POST[username]
+    }
 }
 
 header('Cache-Control: no-cache, must-revalidate');
