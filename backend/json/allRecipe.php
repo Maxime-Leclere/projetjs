3c9f92bc5f9c8310e37c7586a37af10238aa254e
maxime-leclere <maxleclere1999@gmail.com>
2020-03-10 15:25:28 +0100

    list cocktail

--- backend/json/allRecipe.php
+++ backend/json/allRecipe.php
@@ -18,7 +18,8 @@ if(sizeof($data) != 0) {
     for ($i=0; $i < sizeof($data); $i++) {
         if ($idInter  == $data[$i]['idC']) {
             $listRecipe->recipe[$idInter] = array($data[$i]['idC'], $data[$i]['title'],
-                $data[$i]['description_C'], $data[$i]['detail'], 'ingredients' => array(array(
+                $data[$i]['description_C'], $data[$i]['detail'], 'ingredients' => array(
+                    $data[$i]['idI'] => array(
                     $data[$i]['idI'], $data[$i]['description_I']), array(
                     $data[$i]['idU'], $data[$i]['description_U']), $data[$i]['quantity']));
         } else {
