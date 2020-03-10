3f0aa8dd8b35adc1802100f7495260984d943262
maxime-leclere <maxleclere1999@gmail.com>
2020-03-10 15:47:00 +0100

    list cocktail

--- backend/json/allRecipe.php
+++ backend/json/allRecipe.php
@@ -12,7 +12,6 @@ $req = $db->prepare("SELECT * FROM `COCKTAILINGREDIENTUNIT` R, COCKTAIL C,
 
 $req->execute();
 $data = $req->fetchAll();
-print_r( $data);
 if(sizeof($data) != 0) {
     $idInter = 0;
     for ($i=0; $i < sizeof($data); $i++) {
