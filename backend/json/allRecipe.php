56d45df14c38e5e4fbed5d42845a5ca4faa15e85
maxime-leclere <maxleclere1999@gmail.com>
2020-03-11 11:31:13 +0100

    list cocktail

--- backend/json/allRecipe.php
+++ backend/json/allRecipe.php
@@ -20,9 +20,9 @@ if(sizeof($data) != 0) {
                 $data[$i]['description_C'], $data[$i]['detail'], "ingredients" => array());
                 // array_push($listRecipe->recipe[$idCocktail][])
                 $listRecipe->recipe[$idCocktail]['ingredients'][] =
-                    $i => array(array(
+                    array(array(
                     $data[$i]['idI'], $data[$i]['description_I']), array(
-                    $data[$i]['idU'], $data[$i]['description_U']), $data[$i]['quantity'])));
+                    $data[$i]['idU'], $data[$i]['description_U']), $data[$i]['quantity']);
                     echo "yes ".$data[$i]['idC']." ".$idCocktail;
                 print_r($listRecipe->recipe[$idCocktail]['ingredients']);
         } else {
