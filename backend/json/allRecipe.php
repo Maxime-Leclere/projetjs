fe399316794e97d42708c02c72e61297d684d507
maxime-leclere <maxleclere1999@gmail.com>
2020-03-11 11:20:46 +0100

    list cocktail

--- backend/json/allRecipe.php
+++ backend/json/allRecipe.php
@@ -13,21 +13,20 @@ $req = $db->prepare("SELECT * FROM `COCKTAILINGREDIENTUNIT` R, COCKTAIL C,
 $req->execute();
 $data = $req->fetchAll();
 if(sizeof($data) != 0) {
-    $idInter = 1;
+    $idCocktail = 1;
     for ($i=0; $i < sizeof($data); $i++) {
-        if ($idInter  == intval($data[$i]['idC']) || $idInter == 0) {
-            $listRecipe->recipe[$idInter] = array($data[$i]['idC'], $data[$i]['title'],
-                $data[$i]['description_C'], $data[$i]['detail'], 'ingredients' => array());
-                // [] = /*$data[$i]['id_I'] =>*/
-                array_push($listRecipe->recipe[$idInter]['ingredients'], array(array(
+        if ($idCocktail  == intval($data[$i]['idC']) || $idInter == 0) {
+            $listRecipe->recipe[$idCocktail] = array($data[$i]['idC'], $data[$i]['title'],
+                $data[$i]['description_C'], $data[$i]['detail'], $i => array(
+                    array(array(
                     $data[$i]['idI'], $data[$i]['description_I']), array(
                     $data[$i]['idU'], $data[$i]['description_U']), $data[$i]['quantity']));
-                    echo "yes ".$i." ".$idInter;
-                print_r($listRecipe->recipe[$idInter]['ingredients']);
+                    echo "yes ".$i." ".$idCocktail;
+                print_r($listRecipe->recipe[$idCocktail]['ingredients']);
         } else {
-            $idInter++;
+            $idCocktail++;
             $i--;
-            echo "no ".$i." ".$idInter;
+            echo "no ".$i." ".$idCocktail;
 
         }
 
