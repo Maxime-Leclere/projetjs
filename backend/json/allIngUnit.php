1241e19c5e30d3edb1bb11991cbef84fd2525b2b
maxime-leclere <maxime.leclere@etu.univ-amu.fr>
2020-04-02 18:43:21 +0200

    ajout liste ingredient dans le form

--- backend/json/allIngUnit.php
+++ backend/json/allIngUnit.php
@@ -18,13 +18,13 @@ $dataU = $reqU->fetchAll();
 
 if(sizeof($dataI) != 0) {
     for ($i=0; $i < sizeof($dataI); $i++) {
-        $listIngrUnit->list["ingredient"][] = array($dataI["idU"], $dataI["description_I"]);
+        $listIngrUnit->list["ingredient"][] = array($dataI[$i]["idU"], $dataI[$i]["description_I"]);
     }
 }
 
 if(sizeof($dataU) != 0) {
     for ($i=0; $i < sizeof($dataU); $i++) {
-        $listIngrUnit->list["unite"][] = array($dataU["idU"], $dataU["description_U"]);
+        $listIngrUnit->list["unite"][] = array($dataU[$i]["idU"], $dataU[$i]["description_U"]);
     }
 }
 
