f9e3b546e38585fbd24a6078fe40d150bf221ec4
maxime-leclere <maxime.leclere@etu.univ-amu.fr>
2020-04-07 18:33:18 +0200

    ajout insert unit ingr

--- backend/json/insertUnit.php
+++ backend/json/insertUnit.php
@@ -12,4 +12,3 @@ $req->execute();
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
 header('Content-type: application/json');
-(`description_U`) VALUES ()
