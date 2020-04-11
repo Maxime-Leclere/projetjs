9d8b32ef7edac88461e0fed100c146f48fb634da
maxime-leclere <maxime.leclere@etu.univ-amu.fr>
2020-04-11 19:33:42 +0200

    ajout insert cocktail

--- backend/json/insertCocktail.php
+++ backend/json/insertCocktail.php
@@ -3,17 +3,38 @@ require_once '../PDOFactory.php';
 
 $obj = new stdClass();
 $obj->success = true;
-$obj->error = "";
+$obj->error = "Impossible d'avoir des doublons d'ingrédients";
 
+$title = $_POST['title'];
 $quantityIng = $_POST['quantityIng'];
+
 $ingList = array();
 
 for ($i=0; $i < $quantityIng; $i++) {
     $ingList[] = $_POST['inglist'+$i];
 }
-if (sizeof(array_unique($ingList)) != sizeof($ingList)) { // si il y a des doublons
-    $obj->success = false;
-    $obj->error = "Impossible d'avoir des doublons d'ingrédients";
-}
 
 $db = PDOFactory::getConnexion();
+$reqS = $db->prepare('SELECT title FROM COCKTAIL WHERE title = ' + $title);
+$reqS->execute();
+$result = $reqS->fetchAll();
+
+if (sizeof(array_unique($ingList)) != sizeof($ingList)) { // si il y a des doublons
+    $obj->success = false;
+} if(sizeof($result)) {
+    $obj->success = false;
+    $obj->error = "Ce titre est déjà utilisé";
+} if ($obj->success) {
+    $descr = $_POST['description_C'];
+    $detail = $_POST['detail'];
+    $reqICocktail = $db->prepare("INSERT INTO `COCKTAIL`(`title`, `description_C`, `detail`)
+                                VALUES (\"$title\", \"$descr\", \"$detail\")");
+    $reqICocktail->execute();
+}
+
+
+header('Cache-Control: no-cache, must-revalidate');
+header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: application/json');
+
+echo json_encode($obj);
