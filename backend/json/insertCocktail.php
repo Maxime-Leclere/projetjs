509883d74c0bc333a4c887bef2d7dae8491486f9
maxime-leclere <maxime.leclere@etu.univ-amu.fr>
2020-04-11 20:08:58 +0200

    ajout insert cocktail

--- backend/json/insertCocktail.php
+++ backend/json/insertCocktail.php
@@ -10,10 +10,10 @@ $quantityIng = $_POST['quantityIng'];
 $ingList = array();
 
 for ($i=0; $i < $quantityIng; $i++) {
-    $ingList[] = $_POST['inglist'+$i];
+    $ingList[] = $_POST['inglist'.$i];
 }
 $test = 0;
-var_dump($_POST['inglist'+strval($test)]);
+var_dump($_POST['inglist'.strval($test)]);
 var_dump( $ingList);
 
 $db = PDOFactory::getConnexion();
